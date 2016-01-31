ae0c34642ea719ef5bbfac8d443fa970f63babcf
bgyee <bgyee@ucalgary.ca>
2016-01-30 23:13:50 -0700

    more comments

--- assign1/point3D.h
+++ assign1/point3D.h
@@ -1,9 +1,9 @@
 #include<GLFW/glfw3.h>
 
-class Point3D
+class Point3D	// your basic Point3D class
 {
 public:
-	float x;
+	float x;	// point coordinates
 	float y;
 	float z;
 
@@ -14,7 +14,7 @@ public:
         this->z = z;
     }
 
-    Point3D(const Point3D &p)
+    Point3D(const Point3D &p)	// copy constructor
     {
         this->x = p.x;
         this->y = p.y;
